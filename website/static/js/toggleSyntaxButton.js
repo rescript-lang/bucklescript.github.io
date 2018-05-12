7f8f9c04a6279f910f530d38852a9db292213b2a
Rusty Key <iam@stfoo.ru>
2018-05-12 22:55:04 +0200

    store previous chosen syntax in localstorage

--- website/static/js/toggleSyntaxButton.js
+++ website/static/js/toggleSyntaxButton.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const CLASS_REASON = 'syntax__reason'
+  const SYNTAXES = ['reason', 'ocaml']
+  const CLASS_PREFIX = 'syntax__'
   const $toggleSyntaxButton = document.createElement('button')
 
   $toggleSyntaxButton.classList.add('button', 'toggleSyntaxButton')
@@ -7,10 +8,20 @@ document.addEventListener('DOMContentLoaded', () => {
     <span class="toggleSyntaxButton-reason">Reason</span>
     <span class="toggleSyntaxButton-ocaml">OCaml</span>
   `
-
   document.body.appendChild($toggleSyntaxButton)
 
+  let currentSyntax
+  let setCurrentSyntax = (syntax = SYNTAXES[0]) => {
+    document.body.classList.remove(`${CLASS_PREFIX}${currentSyntax}`)
+    document.body.classList.add(`${CLASS_PREFIX}${syntax}`)
+
+    currentSyntax = syntax
+    localStorage.setItem('syntax', currentSyntax)
+  }
+
+  setCurrentSyntax(localStorage.getItem('syntax', currentSyntax))
+
   $toggleSyntaxButton.addEventListener('click', () => {
-    document.body.classList.toggle(CLASS_REASON)
+    setCurrentSyntax(currentSyntax == SYNTAXES[0] ? SYNTAXES[1] : SYNTAXES[0])
   })
 })
